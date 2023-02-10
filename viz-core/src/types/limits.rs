2ce66a302775bfcf8da447b2bfc6b8b16f7d29fc
Fangdun Tsai <cfddream@gmail.com>
2023-02-10 09:39:18 +0800

    chore(core): use static const

--- viz-core/src/types/limits.rs
+++ viz-core/src/types/limits.rs
@@ -25,10 +25,10 @@ impl Default for Limits {
             .insert("text", Limits::NORMAL);
 
         #[cfg(feature = "json")]
-        let limits = limits.insert("json", <Json as Payload>::LIMIT);
+        let limits = limits.insert(<Json as Payload>::NAME, <Json as Payload>::LIMIT);
 
         #[cfg(feature = "form")]
-        let limits = limits.insert("form", <Form as Payload>::LIMIT);
+        let limits = limits.insert(<Form as Payload>::NAME, <Form as Payload>::LIMIT);
 
         limits.sort()
     }
